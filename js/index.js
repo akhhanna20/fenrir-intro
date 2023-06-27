f59a585e2db3f3c199e2a1a2873ed44327694f0c
Hanna <akhhanna20@gmail.com>
2023-06-27 19:01:48 -0400

    Added index.css

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const thisYear = today.getFullYear();
 
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
-copyright.innerHTML = `Hanna Akhramchuk ${thisYear}`;
+copyright.innerHTML = `Hanna Akhramchuk  \u00A9 ${thisYear}`;
 footer.appendChild(copyright);
 
 const skills = [
@@ -28,3 +28,36 @@ for (i = 0; i < skills.length; i++) {
   console.log(skill);
   skillsList.appendChild(skill);
 }
+const messageForm = document.querySelector("[name='leave_message']");
+console.log(messageForm);
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputName = e.target.usersName;
+  console.log(inputName.value);
+  const inputEmail = e.target.usersEmail;
+  console.log(inputEmail.value);
+  const inputMessage = e.target.usersMessage;
+  console.log(inputMessage.value);
+
+  const messageSection = document.querySelector("#messages");
+  console.log(messageSection);
+  const messageList = messageSection.querySelector("ul");
+  console.log(messageList);
+  const newMessage = document.createElement("li");
+  newMessage.innerHTML = `<a href="mailto:${inputEmail.value}">${inputName.value}</a> wrote: <span> ${inputMessage.value} </span>`;
+
+  messageForm.reset();
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "Remove";
+  removeButton.type = "button";
+  removeButton.className = "btnRemove";
+
+  removeButton.addEventListener("click", (e) => {
+    const entry = e.target.parentNode;
+    messageList.removeChild(entry);
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+});
