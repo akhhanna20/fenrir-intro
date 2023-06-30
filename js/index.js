5c96b68803b9927e5a3e5c022641c16ebd139f2b
Hanna <akhhanna20@gmail.com>
2023-06-29 22:36:32 -0400

    Changed lesson-4-4

--- js/index.js
+++ js/index.js
@@ -1,15 +1,9 @@
 const today = new Date();
 const thisYear = today.getFullYear();
-
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
- lesson-4-4
-copyright.innerHTML = `Hanna Akhramchuk  \u00A9 ${thisYear}`;
-
-copyright.innerHTML = `Hanna Akhramchuk \u00A9 ${thisYear}`;
- main
+copyright.innerHTML = `Hanna Akhramchuk ${thisYear}`;
 footer.appendChild(copyright);
-
 const skills = [
   "Java Script",
   "React framework",
@@ -22,20 +16,15 @@ const skills = [
   "Creativity",
   "Time managment",
 ];
-
 const skillsSection = document.querySelector("#skills");
 const skillsList = skillsSection.querySelector("ul");
-
 for (i = 0; i < skills.length; i++) {
   const skill = document.createElement("li");
   skill.innerHTML = skills[i];
   console.log(skill);
   skillsList.appendChild(skill);
 }
- lesson-4-4
 
-
- main
 const messageForm = document.querySelector("[name='leave_message']");
 console.log(messageForm);
 messageForm.addEventListener("submit", (e) => {
@@ -57,14 +46,9 @@ messageForm.addEventListener("submit", (e) => {
   messageForm.reset();
 
   const removeButton = document.createElement("button");
- lesson-4-4
   removeButton.innerText = "Remove";
   removeButton.type = "button";
   removeButton.className = "btnRemove";
-
-  removeButton.innerText = "remove";
-  removeButton.type = "button";
- main
 
   removeButton.addEventListener("click", (e) => {
     const entry = e.target.parentNode;
