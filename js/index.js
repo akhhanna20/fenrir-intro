7da3c17171930a8f73cc5a126fe382671f4fa243
Hanna <akhhanna20@gmail.com>
2023-07-04 09:38:01 -0400

    Changes styles and added Queries

--- js/index.js
+++ js/index.js
@@ -1,12 +1,15 @@
+// Insert Copyright Text in Footer
 const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 const copyright = document.createElement("p");
-copyright.innerHTML = `Hanna Akhramchuk ${thisYear}`;
+copyright.innerHTML = `Hanna Akhramchuk  \u00A9 ${thisYear}`;
 footer.appendChild(copyright);
+
+//Create list of skills
 const skills = [
-  "Java Script",
-  "React framework",
+  "JavaScript",
+  "ReactJS",
   "DOM",
   "APIs",
   "HTML, CSS",
@@ -16,15 +19,18 @@ const skills = [
   "Creativity",
   "Time managment",
 ];
+
 const skillsSection = document.querySelector("#skills");
 const skillsList = skillsSection.querySelector("ul");
 for (i = 0; i < skills.length; i++) {
   const skill = document.createElement("li");
+  skill.className = "skill";
   skill.innerHTML = skills[i];
   console.log(skill);
   skillsList.appendChild(skill);
 }
 
+//Create message form
 const messageForm = document.querySelector("[name='leave_message']");
 console.log(messageForm);
 messageForm.addEventListener("submit", (e) => {
@@ -36,6 +42,7 @@ messageForm.addEventListener("submit", (e) => {
   const inputMessage = e.target.usersMessage;
   console.log(inputMessage.value);
 
+  //create new messade
   const messageSection = document.querySelector("#messages");
   console.log(messageSection);
   const messageList = messageSection.querySelector("ul");
@@ -45,6 +52,7 @@ messageForm.addEventListener("submit", (e) => {
 
   messageForm.reset();
 
+  //Create button to remove message
   const removeButton = document.createElement("button");
   removeButton.innerText = "Remove";
   removeButton.type = "button";
